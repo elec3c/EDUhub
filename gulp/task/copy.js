cef72909770f023b1a87765492addbcfcfa7f1ac
hlusnya <50826390+hlusnya@users.noreply.github.com>
2022-07-25 15:15:17 +0300

    start

---
+++ gulp/task/copy.js
@@ -0,0 +1,6 @@
+export const copy = () => {
+    return app.gulp.src(app.path.src.files)
+    .pipe(app.gulp.dest(app.path.build.files))
+    .pipe(app.gulp.src(app.path.src.dir_root))
+    .pipe(app.gulp.dest(app.path.build.dir_root))
+}
